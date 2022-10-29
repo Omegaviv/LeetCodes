14757291bee2f20826e88418d877d64dd5197dbe
Vivek Verma <86337946+Omegaviv@users.noreply.github.com>
2022-10-29 13:26:14 +0530

    Time: 6 ms (17.61%), Space: 6.1 MB (21.52%) - LeetHub

---
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int isLeapYear(int y){
+        
+        return (y%4==0 && y%100!=0) || y%400==0;
+    }
+    int dayInMonth(int y, int m){
+        if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12) return 31;
+        if(m==2){
+            if(isLeapYear(y)) return 29;
+            return 28;
+        }
+        return 30;
+    }
+    int daysBetweenDates(string date1, string date2) {
+        int y1 = stoi(date1.substr(0, 4));
+        int m1 = stoi(date1.substr(5, 2));
+        int d1 = stoi(date1.substr(8, 2));
+        
+        int y2 = stoi(date2.substr(0, 4));
+        int m2 = stoi(date2.substr(5, 2));
+        int d2 = stoi(date2.substr(8, 2));
+        
+        
+        int days = 0;
+        
+        while(m1!=1){
+            days-=dayInMonth(y1,m1-1);
+            m1--;
+        }
+        days-=d1;
+        
+        while(m2!=1){
+            days+=dayInMonth(y2,m2-1);
+            m2--;
+        }
+        
+        days+=d2;
+        if(y1>y2){
+            days=-days;
+            y1 = y1^y2;
+            y2 = y1^y2;
+            y1 = y1^y2;
+        }
+        while(y1!=y2){
+    
+            if(isLeapYear(y1)) days+=366;
+            else days+=365;
+            y1++;
+        }
+        
+        return abs(days);
+        
+        
+        
+        
+    }
+};
